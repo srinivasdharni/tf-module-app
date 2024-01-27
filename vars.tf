d367539f6599d987e4a5731a6f329e6b5691c2de
India <India@India-PC>
2024-01-28 00:21:10 +0530

    m

--- vars.tf
+++ vars.tf
@@ -1,14 +1,14 @@
-	variable "env" {}
-	variable "component" {}
-	variable "instance_type" {}
-	variable "vpc_id" {}
-	variable "app_port" {}
-	variable "vpc_cidr" {}
-	variable "subnets" {}
-	variable "bastion_node_cidr" {}
-	variable "instance_count" {}
-	variable "prometheus_cidr" {}
-	variable "alb_dns_name" {}
-    variable "zone_id" {}
-    variable "listener_arn" {}
-    variable "listener_rule_priority" {}
+variable "env" {}
+variable "component" {}
+variable "instance_type" {}
+variable "vpc_id" {}
+variable "app_port" {}
+variable "vpc_cidr" {}
+variable "subnets" {}
+variable "bastion_node_cidr" {}
+variable "instance_count" {}
+variable "prometheus_cidr" {}
+variable "alb_dns_name" {}
+variable "zone_id" {}
+variable "listener_arn" {}
+variable "listener_rule_priority" {}
